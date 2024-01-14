e9b174ac08064e4499c02ab7e02c999d4985df03
brunols7 <bruno.lsilva1508@gmail.com>
2024-01-13 22:55:46 -0300

    RestAPICars

---
+++ src/main/java/com/nxt/restapicrud/RestApiCrudApplication.java
@@ -0,0 +1,13 @@
+package com.nxt.restapicrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nxt/restapicrud/RestApiCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nxt.restapicrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
